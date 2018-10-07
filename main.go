a89b8f6a9aead4047927b640264878c84edf635d
Kevin Teh <kkwteh@gmail.com>
2018-10-07 23:06:09 +0200

    Remove example api code

--- main.go
+++ main.go
@@ -5,19 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
-
-//User defines model for storing account details in database
-type User struct {
-	Username  string
-	Password  string `json:"-"`
-	IsAdmin   bool
-	CreatedAt time.Time
-}
 
 type SortRequest struct {
 	BoardCards []string    `json:"board_cards"`
@@ -32,10 +23,6 @@ type SortResponse struct {
 func main() {
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
-	router.HandleFunc("/message", handleQryMessage).Methods("GET")
-	router.HandleFunc("/m/{msg}", handleUrlMessage).Methods("GET")
-
-	router.HandleFunc("/echo", echoHandler).Methods("POST")
 	router.HandleFunc("/sortcards", sortHandler).Methods("POST", "OPTIONS")
 
 	headersOk := handlers.AllowedHeaders([]string{"Authorization", "Content-Type"})
@@ -46,48 +33,8 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8001", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
 
-func handleQryMessage(w http.ResponseWriter, r *http.Request) {
-	vars := r.URL.Query()
-	message := vars.Get("msg")
-
-	json.NewEncoder(w).Encode(map[string]string{"message": message})
-}
-
-func handleUrlMessage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	message := vars["msg"]
-
-	json.NewEncoder(w).Encode(map[string]string{"message": message})
-}
-
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Still alive!")
-}
-
-func echoHandler(w http.ResponseWriter, r *http.Request) {
-	user := User{} //initialize empty user
-
-	//Parse json request body and use it to set fields on user
-	//Note that user is passed as a pointer variable so that it's fields can be modified
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		panic(err)
-	}
-
-	//Set CreatedAt field on user to current local time
-	user.CreatedAt = time.Now().Local()
-
-	//Marshal or convert user object back to json and write to response
-	userJson, err := json.Marshal(user)
-	if err != nil {
-		panic(err)
-	}
-
-	//Set Content-Type header so that clients will know how to read response
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	//Write json response back to response
-	w.Write(userJson)
 }
 
 func sortHandler(w http.ResponseWriter, r *http.Request) {
@@ -114,10 +61,8 @@ func sortHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	//Set Content-Type header so that clients will know how to read response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	//Write json response back to response
 	w.Write(responseJSON)
 }
 
